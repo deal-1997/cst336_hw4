2020027c8d12604d5a8b3328be2a2ced4041ba59
EC2 Default User <ec2-user@ip-172-31-73-127.ec2.internal>
2020-11-25 04:00:56 +0000

    adding changes to index files

--- app.js
+++ app.js
@@ -8,23 +8,23 @@ app.use(express.static("public"));
 
 //routes
 app.get("/", function(req, res){
-    res.render("index.php", {"randomName": randomName});
+    res.render("index.html", {"randomName": randomName});
 });
 
 app.get("/c++", function(req, res){
-    res.render("c++.php", {"randomName": randomName});
+    res.render("c++.html", {"randomName": randomName});
 });
 
 app.get("/java", function(req, res){
-    res.render("java.php", {"randomName": randomName});
+    res.render("java.html", {"randomName": randomName});
 });
 
 app.get("/javascript", function(req, res){
-    res.render("javascript.php", {"randomName": randomName});
+    res.render("javascript.html", {"randomName": randomName});
 });
 
 app.get("/python", function(req, res){
-    res.render("python.php", {"randomName": randomName});
+    res.render("python.html", {"randomName": randomName});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
